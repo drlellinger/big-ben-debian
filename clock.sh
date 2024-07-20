13c1f18ba6cd45daf04a5563f04e7ae17d37798c
drlellinger <39723641+drlellinger@users.noreply.github.com>
2024-07-20 14:48:59 +0200

    added SCRIPTDIR invocation

--- clock.sh
+++ clock.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #Deklaration der Variablen:
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 minute=$(date +"%M")
 hour=$(date +"%I")
 stunde=$(date +"%H")
@@ -7,20 +8,20 @@ tag=$(date +"%d")
 monat=$(date +"%m")
 	if [[ $minute == 30 ]]
 	then
-	  mplayer -ao alsa "/home/pi/big-ben/half-hour.mp3"
+	  mplayer -ao alsa $SCRIPT_DIR/half-hour.mp3
 	elif [[ $minute == 15 ]]
 	then
-	  mplayer -ao alsa "/home/pi/big-ben/quarter-hour.mp3"
+	  mplayer -ao alsa $SCRIPT_DIR/quarter-hour.mp3
 	elif [[ $minute == 45 ]]
 	then
-	  mplayer -ao alsa "/home/pi/big-ben/3quarter-hour.mp3"
+	  mplayer -ao alsa $SCRIPT_DIR/3quarter-hour.mp3
 	elif [[ $minute == 00 ]]
 	then
-	  mplayer -ao alsa "/home/pi/big-ben/full-hour.mp3"
+	  mplayer -ao alsa $SCRIPT_DIR/full-hour.mp3
 	  x=0
 	  while [ $x -lt $hour ]
 	  do
-	    mplayer -ao alsa "/home/pi/big-ben/strike.mp3"
+	    mplayer -ao alsa $SCRIPT_DIR/strike.mp3
 	    let x=x+1
 	  done
 	fi
--- test.sh
+++ test.sh
@@ -1,12 +1,11 @@
-#stunde=$(date +"%H")
-#if [[ $stunde >= 08 -o  $stunde <= 22 ]];
-#then
-mplayer -ao alsa "/home/pi/big-ben/full-hour.mp3"
+#!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+mplayer -ao alsa $SCRIPT_DIR/full-hour.mp3
 hour=$(date +"%I")
 x=0
 while [ $x -lt $hour ]
 do
-    mplayer -ao alsa "/home/pi/big-ben/strike.mp3"
+    mplayer -ao alsa $SCRIPT_DIR/strike.mp3
     let x=x+1
 done
 #fi
